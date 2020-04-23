f49c70f7710443a913b05a2cc6b455fcfee9e5a7
Anand Rud <anandr72@gmail.com>
2020-04-23 13:33:36 +0530

    Sorting out New Line character issues

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Shell Script to deploy Spring-Boot war file
-
-nohup java -jar SpringBootMavenExample-1.3.5.RELEASE.war >> app.log &
--- kill-boot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Shell Script to Kill all Java Process before fresh Deployment
-
-PID=`ps -eaf | grep "java -jar" | grep -v grep | awk '{print $2}'`
-if [[ "" !=  "$PID" ]]; then
-  echo "found $PID and killing it!!"
-  kill -9 $PID
-fi
